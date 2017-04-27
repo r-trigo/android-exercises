e96eb1eba13db5311e101430854b34de60d8992b
ruimigueltrigo <ruimigueltrigo@gmail.com>
2017-04-27 11:11:32 +0100

    gps location updates

--- FusedLocation/app/src/main/java/pt/edp/fusedlocation/FindLocationActivity.java
+++ FusedLocation/app/src/main/java/pt/edp/fusedlocation/FindLocationActivity.java
@@ -12,16 +12,19 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class FindLocationActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+import java.text.DateFormat;
+import java.util.Date;
+
+public class FindLocationActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     //Permissions
     private MyPermissions myPermissions = new MyPermissions();
     // LogCat tag
     private static final String TAG = MainActivity.class.getSimpleName();
-    private int REQUEST_CODE_RECOVER_PLAY_SERVICES = 101;
     private Location mLastLocation;
     // Google client to interact with Google API
     private GoogleApiClient mGoogleApiClient;
@@ -45,8 +48,11 @@ public class FindLocationActivity extends AppCompatActivity implements GoogleApi
         btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
         btnStartLocationUpdates = (Button) findViewById(R.id.btnLocationUpdates);
 
-        // Building the GoogleApi client
-        buildGoogleApiClient();
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            buildGoogleApiClient();
+            createLocationRequest();
+        }
 
         // Show location button click listener
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
@@ -55,11 +61,42 @@ public class FindLocationActivity extends AppCompatActivity implements GoogleApi
                 displayLocation();
             }
         });
+
+        btnStartLocationUpdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                togglePeriodicLocationUpdates();
+            }
+        });
     }
 
-    /**
-     * Method to display the location on UI
-     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Resuming the periodic location updates
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
     private void displayLocation() {
         if (myPermissions.checkLocationPermission(FindLocationActivity.this)) {
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
@@ -74,50 +111,71 @@ public class FindLocationActivity extends AppCompatActivity implements GoogleApi
         }
     }
 
-    /**
-     * Creating google api client object
-     */
+    private void togglePeriodicLocationUpdates() {
+        if (!mRequestingLocationUpdates) {
+            // Changing the button text
+            btnStartLocationUpdates.setText(getString(R.string.btn_stop_location_updates));
+            mRequestingLocationUpdates = true;
+            // Starting the location updates
+            startLocationUpdates();
+            Log.d(TAG, "Periodic location updates started!");
+        } else {
+            // Changing the button text
+            btnStartLocationUpdates.setText(getString(R.string.btn_start_location_updates));
+            mRequestingLocationUpdates = false;
+            // Stopping the location updates
+            stopLocationUpdates();
+            Log.d(TAG, "Periodic location updates stopped!");
+        }
+    }
+
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+                .addApi(LocationServices.API).build();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT); // 10 meters
+    }
+
+    protected void startLocationUpdates() {
+        if (myPermissions.checkLocationPermission(FindLocationActivity.this))
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
+    public void onConnected(Bundle arg0) {
+        displayLocation();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    /**
-     * Google api callback methods
-     */
     @Override
     public void onConnectionFailed(ConnectionResult result) {
         Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
     }
 
     @Override
-    public void onConnected(Bundle arg0) {
-        // Once connected with google api, get the location
-        displayLocation();
-    }
-
-    @Override
     public void onConnectionSuspended(int arg0) {
         mGoogleApiClient.connect();
+    }
+
+    public void onLocationChanged(Location location) {
+        // Assign the new location
+        mLastLocation = location;
+        Toast.makeText(getApplicationContext(), "Location changed!", Toast.LENGTH_SHORT).show();
+        // Displaying the new location on UI
+        displayLocation();
     }
 }
